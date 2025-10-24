c2ceea03966ea4102dd0382e368a50ab458668f2
DrewChen24 <drewchen24@gmail.com>
2025-10-24 11:28:15 +0800

    fix:刪除不必要樣式並新增recommend.scss、套用通用按鈕元件及Swiper統一npm套件

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,7 @@
-import '/assets/scss/index.scss';
 import 'bootstrap/dist/js/bootstrap.min.js';
+import Swiper from 'swiper';
+import { Navigation, Pagination, Autoplay } from 'swiper/modules';
+import '/assets/scss/index.scss';
 
 const columnTags = document.querySelectorAll('#column-filter button');
 const columnItems = document.querySelectorAll('.column-item');
@@ -19,4 +21,47 @@ columnTags.forEach(tag => {
       }
     });
   });
+});
+
+const newsSwiper = new Swiper('.newsSwiper', {
+  modules: [Pagination, Autoplay],
+  direction: 'horizontal',
+  loop: true,
+  autoHeight: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
+
+const productSwiper = new Swiper('.productSwiper', {
+  direction: 'horizontal',
+  loop: false,
+
+  modules: [Navigation],
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  watchOverflow: false, // 當滑塊不足以滾動時自動隱藏導航
+
+  breakpoints: {
+    // RWD 響應式
+    0: {
+      slidesPerView: 'auto',
+      spaceBetween: 12,
+      freeMode: true,
+      navigation: false, // 關閉箭頭
+    },
+    1024: {
+      slidesPerView: 4,
+      spaceBetween: 24,
+      freeMode: false,
+    },
+  },
 });
